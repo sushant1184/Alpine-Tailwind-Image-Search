4dfa6dffaa867a77b84218d03380be2f07097b8c
sushant-dasuri <sushant.dasuri@more.global>
2022-12-19 17:32:03 +0530

    Added pixabay api code to js and updated the index file with alpine js properties

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,13 @@
-import '../css/main.css'
+import '../css/main.css';
+
+function imageGallery() {
+    return {
+      images: [],
+      api_key: "32186098-4687e9302085aeaeaed496199",
+      q: "",
+      image_type: "",
+      page: "",
+      per_page: 200,
+    }
+  }
+

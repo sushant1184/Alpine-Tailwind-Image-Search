d3c8bcaa627c179ec587ba5bd3ea144c57bf5fcf
sushant-dasuri <sushant.dasuri@more.global>
2022-12-19 19:26:44 +0530

    Removed the image gallery function code from the file

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,19 +1,8 @@
 import '../css/main.css';
 
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
  
-window.Alpine = Alpine
+window.Alpine = Alpine;
  
-Alpine.start()
-
-function imageGallery() {
-    return {
-      images: [],
-      api_key: "32186098-4687e9302085aeaeaed496199",
-      q: "",
-      image_type: "",
-      page: "",
-      per_page: 200,
-    }
-  }
+Alpine.start();
 
